61577829277c7be605ce1eec8adfbd1b62396038
titislav <girardinthibault@aol.com>
2020-05-04 09:50:33 +0200

    fichier main test + projet

--- Uno.cpp
+++ Uno.cpp
@@ -2,35 +2,27 @@
 //
 
 #include <iostream>
-#include "Fichier.h"
+#include"Deck.h"
 
-using namespace std;
-
+#pragma once
 int main()
 {
-	cout << "Hello Thibault!\n";
+    Deck d;
+    std::cout << "Generation automatiques des cartes\n";
+    d.generateOpalCards();
+    d.generateColoredCards();
+    d.generateHand();
+    
 
-	vector<string> lignes;
-	lignes.push_back("Ligne1");
-	lignes.push_back("Ligne2");
-
-	// Test de la classe "Fichier"
-	Fichier fichier("test.txt", "../");
-
-
-	cout << "Essais ecriture: " << fichier.ecritureLignes(lignes) << endl;	//Retourne 1 si l'écriture s'est bien passée
-	cout << "Erreur: " << fichier.erreur() << endl; //Doit afficher 0
-
-
-	cout << "\nEssais lecture: changement " << fichier.detecteChangement() << endl;	//Doit afficher 1 (car un changement vient d'être fait)
-	vector<string>* lignesl;
-	lignesl = fichier.lectureLignes();
-
-	for (int i = 0; i < lignesl->size(); i++)
-	{
-		cout << lignesl->operator[](i) << " - ";
-		//cout << (&lignesl)[i] << " - ";
-	}
-	cout << "\nErreur: " << fichier.erreur() << endl;
-	cout << "\nEssais lecture: changement " << fichier.detecteChangement() << endl;	//Doit afficher 0 (car la lecture a déja été faite)
 }
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+
+// Astuces pour bien démarrer : 
+//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
+//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
+//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
+//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
+//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
+//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
