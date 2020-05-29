0a1b11487dd15dcf0f13da7f8be9386187d556e3
titislav <girardinthibault@aol.com>
2020-05-29 13:27:20 +0200

    Ajout de couleurs

--- cartes/StaticFunction.cpp
+++ cartes/StaticFunction.cpp
@@ -1,8 +1,18 @@
 #include "StaticFunction.h"
 
+//Méthode permettant de changer la couleur d'affichage du texte console
+void Color(int color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+
+
 // Tableau de valeur char ou string à afficher dans la console 
-char colors[4] = { 'R','B','J','V' };
+char colorsText[4] = { 'R','B','J','V' };
+int colors[4] = {12,11,14,10};
 vector<string> actions = { "+2", "InvSens", "PasseTour" };
+
 
 
 // Methode permettant de convertir les id des cartes jouées / affichées en nom compréhensible par le joueur
@@ -24,36 +34,81 @@ void showCardName(int cardId)
 		{
 			if (deck[cardId]->getNumber() < 10) // si la valeur d'une carte est inférieur à 10 on sait qu'elle n'aura pas d'effet
 			{
-				color = colors[deck[cardId]->getColor() - 1];
+				color = colorsText[deck[cardId]->getColor() - 1];
 				num = deck[cardId]->getNumber();
-				cout << color << num;
+				Color(colors[deck[cardId]->getColor() - 1]);
+				cout << color;
+				Color(7);
+				cout << num;
+				
 			}
 			else // sinon on sait qu'elle a une faculté spéciale.
 			{
-				color = colors[deck[cardId]->getColor() - 1];
-				cout << color << actions[deck[cardId]->getSpecialType()];
+				color = colorsText[deck[cardId]->getColor() - 1];
+				Color(colors[deck[cardId]->getColor() - 1]);
+				cout << color; 
+				Color(7);
+				cout << actions[deck[cardId]->getSpecialType()];
+				
 			}
 		}
 		else // si on touche au cartes supérieur à n - 8 on affiche les subsituts de couleur des cartes +4 et joker 
 		{
-			if (cardId == deck.size()-1)
-				cout << "+4 vert";
-			if (cardId == deck.size()-2)
-				cout << "+4 jaune";
-			if (cardId == deck.size()-3)
-				cout << "+4 bleu";
-			if (cardId == deck.size()-4)
-				cout << "+4 rouge";
-			if (cardId == deck.size()-5)
-				cout << "joker vert";
-			if (cardId == deck.size()-6)
-				cout << "joker jaune";
-			if (cardId == deck.size()-7)
-				cout << "joker bleu";
-			if (cardId == deck.size()-8)
-				cout << "joker rouge";
+			if (cardId == deck.size() - 1)
+			{
+				cout << "+4 ";
+				Color(10);
+				cout << "vert";
+			}
+			if (cardId == deck.size() - 2) 
+			{
+				cout << "+4 ";
+				Color(14);
+				cout<<"jaune";
+			}
+				
+			if (cardId == deck.size() - 3) 
+			{
+				cout << "+4 ";
+				Color(3);
+				cout << "bleu";
+			}
+				
+			if (cardId == deck.size() - 4)
+			{
+				cout << "+4 ";
+				Color(12);
+				cout<<"rouge";
+			}
+			if (cardId == deck.size() - 5)
+			{
+				cout << "joker ";
+				Color(10);
+				cout << "vert";
+			}
+			if (cardId == deck.size() - 6)
+			{
+				cout << "joker "; 
+				Color(14);
+				cout << "jaune";
+			}
+			if (cardId == deck.size() - 7)
+			{
+				cout << "joker ";
+				Color(11);
+				cout << "bleu";
+			}
+			if (cardId == deck.size() - 8)
+			{
+				cout << "joker ";
+				Color(12);
+				cout << "rouge";
+			}
 			
-
+			Color(7);
 		}
 	}
 }
+
+
+
